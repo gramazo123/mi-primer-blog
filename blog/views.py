8ad1562a7ab5188b8f3c9db5529f4200ca278420
Otto <gramazo123@mesoamericana.edu.gt>
2015-10-09 10:25:48 -0600

    Se agregaron vistas para ver los post detallados.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Postear
 
@@ -8,3 +9,6 @@ def post_list(request):
     posts = Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/Listar_publicaciones.html', {'posts': posts})
 
+def detalles_post(request, pk):
+        post = get_object_or_404(Postear, pk=pk)
+        return render(request, 'blog/detalles_post.html', {'post': post})
